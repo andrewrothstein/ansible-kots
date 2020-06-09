35e455e594b151fd0a5aec044b945004e40a0cc6
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-06-08 22:31:11 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+andrewrothstein.kots
+=========
+[![Build Status](https://travis-ci.org/andrewrothstein/ansible-kots.svg?branch=master)](https://travis-ci.org/andrewrothstein/ansible-kots)
+
+Installs [kots](https://kots.io/).
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.kots
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksums.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env sh
+set -e
+#set -x
+DIR=~/Downloads
+MIRROR=https://github.com/replicatedhq/kots/releases/download
+APP=kots
+
+dl() {
+    local ver=$1
+    local lchecksums=$2
+    local os=$3
+    local arch=$4
+    local archive_type=${5:-tar.gz}
+    local platform="${os}_${arch}"
+    local file="${APP}_${platform}.${archive_type}"
+    local url=$MIRROR/v$ver/$file
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(grep $file $lchecksums | awk '{print $1}')
+}
+
+dl_ver() {
+    local ver=$1
+    # https://github.com/replicatedhq/kots/releases/download/v1.16.0/kots_1.16.0_checksums.txt
+    local checksums="${APP}_${ver}_checksums.txt"
+    local url=$MIRROR/v$ver/$checksums
+    local lchecksums="$DIR/${checksums}"
+    if [ ! -e $lchecksums ];
+    then
+        wget -q -O $lchecksums $url
+    fi
+
+    printf "  # %s\n" $url
+    printf "  '%s':\n" $ver
+
+    dl $ver $lchecksums darwin amd64
+    dl $ver $lchecksums linux amd64
+    dl $ver $lchecksums windows amd64 zip
+}
+
+dl_ver ${1:-1.16.0}
