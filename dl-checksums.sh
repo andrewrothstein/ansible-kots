13480d8a6ed4c16ebf225b1366bf5e30e51600dc
Andrew Rothstein <andrew.rothstein@gmail.com>
2022-08-24 00:16:16 -0400

    kots:1.8.1

--- dl-checksums.sh
+++ dl-checksums.sh
@@ -25,14 +25,15 @@ dl_ver() {
     local lchecksums="$DIR/${checksums}"
     if [ ! -e $lchecksums ];
     then
-        wget -q -O $lchecksums $url
+        curl -sSLf -o $lchecksums $url
     fi
 
     printf "  # %s\n" $url
     printf "  '%s':\n" $ver
 
-    dl $ver $lchecksums darwin amd64
+    dl $ver $lchecksums darwin all
     dl $ver $lchecksums linux amd64
+    dl $ver $lchecksums linux arm64
 }
 
-dl_ver ${1:-1.50.1}
+dl_ver ${1:-1.81.1}
